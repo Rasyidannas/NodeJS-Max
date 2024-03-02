44e4c6385d4f3a66000920ed761041ac20e9e815
rasyid annas <rasshit.dsgn@gmail.com>
2024-03-02 14:38:30 +0700

    5/60. Adding Middleware.mp4

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ const express = require("express");
 
 const app = express();
 
+//this for new middleware
+app.use((req, res, next) => {
+  console.log("in the middleware");
+  next(); //this allow request for travel/continue to another middleware or in below
+});
+
+app.use((req, res, next) => {
+  console.log("in another middleware");
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
