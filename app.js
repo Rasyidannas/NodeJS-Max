56685592346efde1f9cb7b8000d937ae9e00827c
rasyid annas <rasshit.dsgn@gmail.com>
2024-03-02 18:01:47 +0700

    5/64. Parsing Incoming Request.mp4

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-//this middleware only for / or all with /
-app.use("/", (req, res, next) => {
-  console.log("this middleware always running");
-  next(); //this will continue to another/below middleware
-});
+//this for parse request in middleware with 3rd part library
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //this middleware only for /add-product url
 app.use("/add-product", (req, res, next) => {
-  console.log("in another middleware");
-  res.send("<h1>The 'Add Product' Page</h1>");
+  //   console.log("in another middleware");
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title" /> <button type="submit">Add Product</button></form>'
+  );
+});
+
+app.use("/product", (req, res) => {
+  console.log(req.body); //this will be output because body-parser
+  res.redirect("/");
 });
 
 //this middleware only for / or all with /
 app.use("/", (req, res, next) => {
-  console.log("in another middleware");
+  //   console.log("in another middleware");
   res.send("<h1>Hello from Express!</h1>");
 });
 
