6591a7a0828ca4ff418cf0a72358a9e76150300b
rasyid annas <rasshit.dsgn@gmail.com>
2024-03-05 15:09:38 +0700

    5/71. Returning a 404 Page.mp4

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require("express");
 
 const adminRoutes = require("./routes/admin");
@@ -17,7 +19,7 @@ app.use(shopRoutes);
 
 //this for 404 page not found
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found!</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
